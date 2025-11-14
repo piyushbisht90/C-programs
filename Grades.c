81d4fe0376847dd8caff26cbfbf79d21bee4fec3
piyushbisht90 <piyushbisht155@gmail.com>
2025-11-14 16:11:02 +0530

    Create Grades.c

---
+++ Grades.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int m; scanf("%d", &m);
+    if (m >= 90) printf("A
+");
+    else if (m >= 80) printf("B
+");
+    else if (m >= 70) printf("C
+");
+    else if (m >= 60) printf("D
+");
+    else printf("F
+");
+    return 0;
+}
