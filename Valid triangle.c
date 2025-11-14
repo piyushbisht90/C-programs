5797e24cf2de8b1dc907305a54e4d3f30a5d2805
piyushbisht90 <piyushbisht155@gmail.com>
2025-11-14 16:13:26 +0530

    Create Valid triangle.c

---
+++ Valid triangle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a,b,c; scanf("%d %d %d", &a, &b, &c);
+    if(a+b+c==180 && a>0 && b>0 && c>0) printf("Valid
+");
+    else printf("Invalid
+");
+    return 0;
+}
